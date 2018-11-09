a6de55a375bea305f1d04b16d63804d8afd22231
Alex <tomattman@gmail.com>
2018-11-09 11:50:44 +0300

    init commit

---
+++ android/src/com/gmail/tomattman/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gmail.tomattman;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gmail.tomattman.StrudyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StrudyGame(), config);
+	}
+}
